8d345e7c00a0bdea5486d51bedd7ba369022a411
William Rory Kronmiller <wrk961@gmail.com>
2018-11-28 06:57:51 -0500

    Add NMAP

--- stats.js
+++ stats.js
@@ -1,12 +1,35 @@
 'use strict';
 
+const { Parser } = require('xml2js');
 const { getS3 } = require('./s3');
 
 const systemDataKey = 'status-files/system-data.json';
+const nmapKey = 'status-files/nmap.xml';
 
 module.exports.getSystem = (event, context, callback) => {
   getS3({ key: systemDataKey }).then(JSON.parse).then(body => {
     callback(null, body);
   })
   .catch(error => callback(error, null));
+};
+
+const parser = new Parser({ explicitRoot: false });
+function parseXML(xml) {
+  return new Promise((resolve, reject) =>
+    parser.parseString(xml, (err, res) => (err) ? reject(err) : resolve(res)))
+    .catch(err => console.error({ err, xml }));
+}
+
+function getNmap() {
+  return getS3({ key: nmapKey })
+    .then(str => str.trim())
+    .then(str => str.replace(/\n/g, ''))
+    .then(str => str + '</nmaprun>') //HACK! nmap outputting malformed xml
+    .then(parseXML)
+}
+
+module.exports.getNmap = (event, context, callback) => {
+  getNmap()
+    .then(body => callback(null, { body }))
+    .catch(error => callback(error, null))
 };
